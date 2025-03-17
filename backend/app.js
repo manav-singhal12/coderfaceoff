fab4de5db198d4f69536a9593867d9a00cadc02b
manav-singhal12 <manavsinghal165@gmail.com>
2025-03-17 16:36:27 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,14 @@ import cookieParser from 'cookie-parser';
 
 const app=express();
 
+
 app.use(cors({
-    origin:"*",
-    credentials:true
+    origin: 'https://solbudget.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Ensure all necessary methods are allowed
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers if needed
 }));
+
 
 app.use(express.json({
     limit:"50kb",
